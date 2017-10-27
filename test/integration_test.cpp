36afcb5d7b9e559841c915a00ce3523d9c24f033
trylock <lothar.gordon@gmail.com>
2017-10-28 01:44:53 +0200

    Refactor integration tests
    Use helper functions assert_error and
    assert_no_error instead of test_error_message.

--- test/integration_test.cpp
+++ test/integration_test.cpp
@@ -17,23 +17,29 @@ struct parse_result
 
     // parser and lexer errors
     std::stringstream errors;
-};
 
-void test_error_message(std::stringstream& stream, const std::string& msg)
-{
-    std::string error_msg;
-    std::getline(stream, error_msg);
-    REQUIRE(error_msg.size() > 0);
-
-    // remove line, column number and "error:" string
-    auto pos = error_msg.find("error:") + 10;
-    while (pos < error_msg.size() && std::isspace(error_msg[pos]))
+    void assert_no_error()
     {
-        ++pos;
+        REQUIRE(errors.peek() == EOF);
     }
-    error_msg = error_msg.substr(pos); 
-    REQUIRE(error_msg == msg);
-}
+
+    void assert_error(const std::string& expected_msg) 
+    {
+        std::string actual_msg; 
+        std::getline(errors, actual_msg);
+        REQUIRE(actual_msg.size() > 0);
+            
+        // remove line, column number and "error:" string
+        auto pos = actual_msg.find("error:") + 10;
+        while (pos < actual_msg.size() && std::isspace(actual_msg[pos]))
+        {
+            ++pos;
+        }
+        actual_msg = actual_msg.substr(pos);
+
+        REQUIRE(actual_msg == expected_msg);
+    }
+};
 
 // Helper function to setup a parser and parse given expression
 static parse_result parse(const std::string& expr)
@@ -82,7 +88,7 @@ TEST_CASE("Interpret a single integer value", "[dice]")
     auto result = parse("42");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -96,7 +102,7 @@ TEST_CASE("Interpret a single double value", "[dice]")
     auto result = parse("3.1415");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_double::id());
@@ -110,8 +116,8 @@ TEST_CASE("Interpret an invalid double value", "[dice]")
     auto result = parse("3.");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Invalid floating point number. Decimal part must not be empty: 3.");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Invalid floating point number. Decimal part must not be empty: 3.");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_double::id());
@@ -125,9 +131,9 @@ TEST_CASE("Skip unknown characters", "[dice]")
     auto result = parse("?!4");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Unexpected character: '?' (0x3F).");
-    test_error_message(result.errors, "Unexpected character: '!' (0x21).");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Unexpected character: '?' (0x3F).");
+    result.assert_error("Unexpected character: '!' (0x21).");
+    result.assert_no_error();
     
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -141,7 +147,7 @@ TEST_CASE("Interpret an arithmetic expression", "[dice]")
     auto result = parse("1 + 2 * 3 / 4 - 5");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -155,7 +161,7 @@ TEST_CASE("Interpret a dice roll expression", "[dice]")
     auto result = parse("1d2d4");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -177,7 +183,7 @@ TEST_CASE("Interpret a function call", "[dice]")
     auto result = parse("expectation(1d6)");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_double::id());
@@ -191,9 +197,9 @@ TEST_CASE("Interpret an expression even if it starts with invalid symbols", "[di
     auto result = parse("* ) 1 + 2 * 3");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors,  "Invalid token at the beginning of an expression: *");
-    test_error_message(result.errors,  "Invalid token at the beginning of an expression: )");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error( "Invalid token at the beginning of an expression: *");
+    result.assert_error( "Invalid token at the beginning of an expression: )");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -208,7 +214,7 @@ TEST_CASE("Interpret a relational operator in", "[dice]")
     auto result = parse("1d6 in [2, 5]");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -224,7 +230,7 @@ TEST_CASE("Interpret a relational operator <", "[dice]")
     auto result = parse("1d6 < 3");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -240,7 +246,7 @@ TEST_CASE("Interpret a relational operator <=", "[dice]")
     auto result = parse("1d6 <= 3");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -256,7 +262,7 @@ TEST_CASE("Interpret a relational operator ==", "[dice]")
     auto result = parse("1d6 == 6");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -272,7 +278,7 @@ TEST_CASE("Interpret a relational operator !=", "[dice]")
     auto result = parse("1d6 != 6");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -288,7 +294,7 @@ TEST_CASE("Interpret a relational operator >=", "[dice]")
     auto result = parse("1d6 >= 5");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -304,7 +310,7 @@ TEST_CASE("Interpret a relational operator >", "[dice]")
     auto result = parse("1d6 > 4");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
     
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -320,9 +326,9 @@ TEST_CASE("Don't interpret the in operator if the lower bound is invalid express
     auto result = parse("1d4 in [, 3]");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Invalid operand for the lower bound of operator in");
-    test_error_message(result.errors, "Expected <end of input>, got ,.");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Invalid operand for the lower bound of operator in");
+    result.assert_error("Expected <end of input>, got ,.");
+    result.assert_no_error();
     
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -340,9 +346,9 @@ TEST_CASE("Don't interpret the in operator if the upper bound is invalid express
     auto result = parse("1d4 in [1, +]");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Invalid operand for the upper bound of operator in");
-    test_error_message(result.errors, "Expected <end of input>, got +.");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Invalid operand for the upper bound of operator in");
+    result.assert_error("Expected <end of input>, got +.");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -360,9 +366,9 @@ TEST_CASE("Don't interpret relational operator if the second operand is invalid"
     auto result = parse("1 < +");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Invalid operand for <relational operator> '<'");
-    test_error_message(result.errors, "Expected <end of input>, got +.");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Invalid operand for <relational operator> '<'");
+    result.assert_error("Expected <end of input>, got +.");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -376,8 +382,8 @@ TEST_CASE("Resume interpreting relational operator after finding a sync symbol",
     auto result = parse("1 < * 2");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Invalid token at the beginning of an addition: *");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Invalid token at the beginning of an addition: *");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -392,9 +398,9 @@ TEST_CASE("Don't interpret the + operator if the second operand is invalid", "[d
     auto result = parse("2 + *");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Invalid operand for binary operator +");
-    test_error_message(result.errors, "Expected <end of input>, got *.");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Invalid operand for binary operator +");
+    result.assert_error("Expected <end of input>, got *.");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -408,8 +414,8 @@ TEST_CASE("Resume interpreting the + operator if we find a sync symbol", "[dice]
     auto result = parse("2 + [ 3");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors,  "Invalid token at the beginning of a multiplication: [");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error( "Invalid token at the beginning of a multiplication: [");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -423,9 +429,9 @@ TEST_CASE("Don't interpret the * operator if the second operand is invalid", "[d
     auto result = parse("2 * )");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors, "Invalid operand for binary operator *");
-    test_error_message(result.errors, "Expected <end of input>, got ).");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error("Invalid operand for binary operator *");
+    result.assert_error("Expected <end of input>, got ).");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -439,8 +445,8 @@ TEST_CASE("Resume interpreting the * operator if we find a sync symbol", "[dice]
     auto result = parse("2 * [ 4");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors,  "Invalid token at the beginning of a dice roll: [");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error( "Invalid token at the beginning of a dice roll: [");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -454,7 +460,7 @@ TEST_CASE("Interpret arithmetic expressing with doubles and ints", "[dice]")
     auto result = parse("1.5 * 2 + 3 - 0.5");
 
     REQUIRE(result.values.size() == 1);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_double::id());
@@ -481,8 +487,8 @@ TEST_CASE("Interpret a function with invalid first argument", "[dice]")
     auto result = parse("add(,1,2)");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors,  "Invalid function parameter 0");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error( "Invalid function parameter 0");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -496,9 +502,9 @@ TEST_CASE("Don't interpret the dice roll operator if its operand is invalid", "[
     auto result = parse("1d)");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors,  "Invalid operand for binary operator D (dice roll)");
-    test_error_message(result.errors,  "Expected <end of input>, got ).");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error( "Invalid operand for binary operator D (dice roll)");
+    result.assert_error( "Expected <end of input>, got ).");
+    result.assert_no_error();
     
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_int::id());
@@ -512,8 +518,8 @@ TEST_CASE("Resume interpreting the dice roll operator if we find a sync symbol",
     auto result = parse("1d[4");
     
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors,  "Invalid token at the beginning of a factor: [");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error( "Invalid token at the beginning of a factor: [");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_rand_var::id());
@@ -531,8 +537,8 @@ TEST_CASE("Resume interpreting an expression in function arguments after finding
     auto result = parse("expectation(+1d4)");
 
     REQUIRE(result.values.size() == 1);
-    test_error_message(result.errors,  "Invalid token at the beginning of an expression: +");
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_error( "Invalid token at the beginning of an expression: +");
+    result.assert_no_error();
 
     auto value = std::move(result.values[0]);
     REQUIRE(value->type() == dice::type_double::id());
@@ -546,7 +552,7 @@ TEST_CASE("Interpret expression with variables", "[dice]")
     auto result = parse("var X = 1d6; (X == 5) * 4 + (1 - (X == 5)) * 2");
 
     REQUIRE(result.values.size() == 2);
-    REQUIRE(result.errors.peek() == EOF);
+    result.assert_no_error();
 
     REQUIRE(result.values[0] == nullptr);
     
