7c75408cd75c976c34fb44512dcbeb683937bd88
trylock <lothar.gordon@gmail.com>
2017-12-10 11:40:55 +0100

    Add checked for unsigned values

--- src/checked.hpp
+++ src/checked.hpp
@@ -7,8 +7,12 @@
 namespace dice
 {
     // Check for arithmetic errors 
+    template<typename T, bool is_signed = std::numeric_limits<T>::is_signed>
+    class checked;
+
+    // signed value specialization
     template<typename T>
-    class checked
+    class checked<T, true>
     {
     public:
         checked() {}
@@ -82,32 +86,41 @@ namespace dice
         {
             using namespace std;
 
-            static_assert(max() + min() == -1);
-
-            if (other_value != 0 && value_ != 0)
+            auto diff = max() + min();
+            if (diff < 0)
             {
                 if (value_ == -1 && other_value == min() ||
                     value_ == min() && other_value == -1)
                 {
                     throw std::overflow_error{ 
-                        to_string(value_) + " * " + to_string(other_value) };
+                        to_string(value_) + " * " + to_string(other_value) 
+                    };
                 }
+            }
+            else if (diff > 0)
+            {
+                if (value_ == -1 && other_value == max() ||
+                    value_ == max() && other_value == -1)
+                {
+                    throw std::underflow_error{
+                        to_string(value_) + " * " + to_string(other_value) 
+                    };
+                }
+            }
 
+            if (other_value != 0 && value_ != 0 && other_value != -1)
+            {
                 // check whether there would be an overflow error
-                auto both_positive = value_ > 0 && other_value > 0;
-                auto both_negative = value_ < 0 && other_value < 0;
-                if (both_positive && value_ > max() / other_value ||
-                    both_negative && value_ < max() / other_value)
+                if (other_value > 0 && value_ > max() / other_value ||
+                    other_value < 0 && value_ < max() / other_value)
                 {
                     throw std::overflow_error{ 
                         to_string(value_) + " * " + to_string(other_value) };
                 }
 
                 // check whether there would be an underflow error
-                // note: if other_value == -1, it can't overflow/underflow
-                //       unless value_ == min which is handled already
                 if (other_value > 0 && value_ < min() / other_value ||
-                    other_value < -1 && value_ > min() / other_value)
+                    other_value < 0 && value_ > min() / other_value)
                 {
                     throw std::underflow_error{ 
                         to_string(value_) + " * " + to_string(other_value) };
@@ -127,18 +140,30 @@ namespace dice
         {
             using namespace std;
 
-            static_assert(max() + min() == -1);
-
             if (other_value == 0)
             {
                 throw std::overflow_error{ "Division by zero: " + 
                     to_string(value_) + " / 0" };
             }
 
-            if (value_ == min() && other_value == -1)
+            auto diff = max() + min();
+            if (diff < 0)
             {
-                throw std::overflow_error{ 
-                    to_string(value_) + " / " + to_string(other_value) };
+                if (value_ == min() && other_value == -1)
+                {
+                    throw std::overflow_error{ 
+                        to_string(value_) + " / " + to_string(other_value) 
+                    };
+                }
+            }
+            else if (diff > 0)
+            {
+                if (value_ == max() && other_value == -1)
+                {
+                    throw std::underflow_error{
+                        to_string(value_) + " / " + to_string(other_value) 
+                    };
+                }
             }
 
             return checked<T>{ value_ / other_value };
@@ -152,11 +177,14 @@ namespace dice
         {
             using namespace std;
 
-            static_assert(max() + min() == -1);
-
-            if (value_ == min())
+            auto diff = max() + min();
+            if (diff < 0 && value_ == min())
             {
                 throw std::overflow_error{ "-" + to_string(value_) };
+            }
+            else if (diff > 0 && value_ == max())
+            {
+                throw std::underflow_error{ "-" + to_string(value_) };
             }
             return checked<T>{ -value_ };
         }
@@ -188,6 +216,135 @@ namespace dice
         static constexpr T min()
         {
             return std::numeric_limits<T>::min();
+        }
+    };
+
+    // unsigned value specialization
+    template<typename T>
+    class checked<T, false>
+    {
+    public:
+        checked() {}
+        checked(const T& value) : value_(value) {}
+
+        // allow copy
+        checked(const checked&) = default;
+        checked& operator=(const checked&) = default;
+
+        // allow move
+        checked(checked&&) = default;
+        checked& operator=(checked&&) = default;
+
+        /** Compute addition of the 2 values.
+         * If an overflow occurs, the std::overflow_error is thrown.
+         * @param other raw value
+         * @return result of the addition as a checked type
+         */
+        auto operator+(const T& other_value) const
+        {
+            using namespace std;
+
+            if (value_ > max() - other_value)
+            {
+                throw std::overflow_error{ 
+                    to_string(value_) + " + " + to_string(other_value) };
+            }
+
+            return checked<T>{ value_ + other_value };
+        }
+
+        /** Compute subtraction of the 2 values.
+         * If an uderflow occurs, the std::underflow_error is thrown.
+         * @param other raw value
+         * @return result of the subtraction as a checked type
+         */
+        auto operator-(const T& other_value) const
+        {
+            using namespace std;
+
+            if (value_ <  other_value)
+            {
+                throw std::underflow_error{ 
+                    to_string(value_) + " - " + to_string(other_value) };
+            }
+
+            return checked<T>{ value_ - other_value };
+        }
+
+        /** Compute multiplication of the 2 values.
+         * If an overflow occurs, the std::overflow_error is thrown.
+         * @param other raw value
+         * @return result of the multiplication as a checked type
+         */
+        auto operator*(const T& other_value) const
+        {
+            using namespace std;
+
+            // check whether there would be an overflow error
+            if (other_value != 0 && value_ > max() / other_value)
+            {
+                throw std::overflow_error{ 
+                    to_string(value_) + " * " + to_string(other_value) };
+            }
+
+            return checked<T>{ value_ * other_value };
+        }
+
+        /** Compute division of the 2 values.
+         * If the other_value is zero, the std::overflow_error is thrown.
+         * @param other raw value
+         * @return result of the division as a checked type
+         */
+        auto operator/(const T& other_value) const
+        {
+            using namespace std;
+
+            if (other_value == 0)
+            {
+                throw std::overflow_error{ "Division by zero: " + 
+                    to_string(value_) + " / 0" };
+            }
+
+            return checked<T>{ value_ / other_value };
+        }
+
+        /** Compute unary minus of the value.
+         * If an overflow occurs, the std::overflow_error is thrown.
+         * @return result of the unary minus as a checked type
+         */
+        auto operator-() const
+        {
+            using namespace std;
+
+            if (value_ != 0)
+            {
+                throw std::underflow_error{ "-" + to_string(value_) };
+            }
+            return checked<T>{ 0 };
+        }
+
+        // cast to type T
+        operator T() const 
+        { 
+            return value_; 
+        }
+
+        // explicit value getter/setter
+        T& value() 
+        { 
+            return value_; 
+        }
+
+        const T& value() const
+        {
+            return value_;
+        }
+    private:
+        T value_;
+
+        static constexpr T max()
+        {
+            return std::numeric_limits<T>::max();
         }
     };
 }
--- test/checked_test.cpp
+++ test/checked_test.cpp
@@ -2,6 +2,8 @@
 
 #include "checked.hpp"
 
+// Test int
+
 TEST_CASE("Add integers in range", "[checked]")
 {
     using namespace dice;
@@ -28,22 +30,6 @@ TEST_CASE("Handle underflow in addition", "[checked]")
 
     REQUIRE_THROWS_AS(checked<int>{ std::numeric_limits<int>::min() } + (-1), std::underflow_error);
     REQUIRE_THROWS_AS(checked<int>{ -1 } + std::numeric_limits<int>::min(), std::underflow_error);
-}
-
-TEST_CASE("Add unsigned integers in range", "[checked]")
-{
-    using namespace dice;
-
-    REQUIRE(checked<std::uint32_t>{ 5 } + std::uint32_t{ 7 } == 12);
-    REQUIRE(checked<std::uint32_t>{ std::numeric_limits<std::uint32_t>::max() } + std::uint32_t{ 0 } == std::numeric_limits<std::uint32_t>::max());
-}
-
-TEST_CASE("Handle overflown in addition for unsigned integers", "[checked]")
-{
-    using namespace dice;
-
-    REQUIRE_THROWS_AS(checked<std::uint32_t>{ std::numeric_limits<std::uint32_t>::max() } + std::uint32_t{ 1 }, std::overflow_error);
-    REQUIRE_THROWS_AS(checked<std::uint32_t>{ std::numeric_limits<std::uint32_t>::max() / 2 } + std::numeric_limits<std::uint32_t>::max() / 2 + std::uint32_t{ 2 }, std::overflow_error);
 }
 
 TEST_CASE("Subtract integers in range", "[checked]")
@@ -151,4 +137,107 @@ TEST_CASE("Hnalde overflow in unary minus", "[checked]")
     using namespace dice;
 
     REQUIRE_THROWS_AS(-checked<int>{ std::numeric_limits<int>::min() }, std::overflow_error);
+}
+
+// Test unsigned int
+
+TEST_CASE("Add integers in range (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto max = std::numeric_limits<std::uint32_t>::max();
+    auto min = std::numeric_limits<std::uint32_t>::min();
+    REQUIRE(checked<std::uint32_t>{ 5 } + std::uint32_t{ 7 } == 12);
+    REQUIRE(checked<std::uint32_t>{ max } + std::uint32_t{ 0 } == max);
+    REQUIRE(checked<std::uint32_t>{ min } + std::uint32_t{ 4 } == min + 4);
+    REQUIRE(checked<std::uint32_t>{ min } + std::uint32_t{ 0 } == min);
+}
+
+TEST_CASE("Handle overflow in addition (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto max = std::numeric_limits<std::uint32_t>::max();
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ max } + std::uint32_t{ 1 }, std::overflow_error);
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ 1 } + max, std::overflow_error);
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ max / 2 } + max / 2 + std::uint32_t{ 2 }, std::overflow_error);
+}
+
+TEST_CASE("Subtract integers in range (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto max = std::numeric_limits<std::uint32_t>::max();
+    auto min = std::numeric_limits<std::uint32_t>::min();
+    REQUIRE(checked<std::uint32_t>{ 7 } - std::uint32_t{ 5 } == 2);
+    REQUIRE(checked<std::uint32_t>{ max } - std::uint32_t{ 4 } == max - 4);
+    REQUIRE(checked<std::uint32_t>{ max } - std::uint32_t{ 0 } == max);
+    REQUIRE(checked<std::uint32_t>{ min } - std::uint32_t{ 0 } == min);
+}
+
+TEST_CASE("Handle underflow in subtraction (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto min = std::numeric_limits<std::uint32_t>::min();
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ min } - std::uint32_t{ 1 }, std::underflow_error);
+}
+
+TEST_CASE("Multiply integers in range (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto max = std::numeric_limits<std::uint32_t>::max();
+    REQUIRE(checked<std::uint32_t>{ 5 } * std::uint32_t{ 10 } == 50);
+    REQUIRE(checked<std::uint32_t>{ 1 } * std::uint32_t{ 1 } == 1);
+    REQUIRE(checked<std::uint32_t>{ max } * std::uint32_t{ 1 } == max);
+    REQUIRE(checked<std::uint32_t>{ max } * std::uint32_t{ 0 } == 0);
+    REQUIRE(checked<std::uint32_t>{ 0 } * max == 0);
+}
+
+TEST_CASE("Handle overflow in multiplication (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto max = std::numeric_limits<std::uint32_t>::max();
+    auto half_max = max / std::uint32_t{ 2 }; 
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ max } * std::uint32_t{ 2 }, std::overflow_error);
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ half_max } * half_max, std::overflow_error);
+}
+
+TEST_CASE("Divide integers in range (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto max = std::numeric_limits<std::uint32_t>::max();
+    REQUIRE(checked<std::uint32_t>{ max } / std::uint32_t{ 1 } == max);
+    REQUIRE(checked<std::uint32_t>{ max } / std::uint32_t{ 2 } == max / 2);
+    REQUIRE(checked<std::uint32_t>{ 0 } / std::uint32_t{ 1 } == 0);
+    REQUIRE(checked<std::uint32_t>{ 0 } / max == 0);
+}
+
+TEST_CASE("Handle division by zero (uint)", "[checked]")
+{
+    using namespace dice;
+
+    auto max = std::numeric_limits<std::uint32_t>::max();
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ 0 } / std::uint32_t{ 0 }, std::overflow_error);
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ 1 } / std::uint32_t{ 0 }, std::overflow_error);
+    REQUIRE_THROWS_AS(checked<std::uint32_t>{ max } / std::uint32_t{ 0 }, std::overflow_error);
+}
+
+TEST_CASE("Unary minus on zero (uint)", "[checked]")
+{
+    using namespace dice;
+
+    REQUIRE(-checked<std::uint32_t>{ 0 } == 0);
+}
+
+TEST_CASE("Hnalde underflow in unary minus (uint)", "[checked]")
+{
+    using namespace dice;
+
+    REQUIRE_THROWS_AS(-checked<std::uint32_t>{ 1 }, std::underflow_error);
+    REQUIRE_THROWS_AS(-checked<std::uint32_t>{ 45 }, std::underflow_error);
+    REQUIRE_THROWS_AS(-checked<std::uint32_t>{ std::numeric_limits<std::uint32_t>::max() }, std::underflow_error);
 }
